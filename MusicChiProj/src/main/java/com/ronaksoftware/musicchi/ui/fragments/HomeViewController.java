9db8a498458330a4a200e706766f738cc7b6e85f
Reza Pilehvar <graphreza@gmail.com>
2020-02-15 16:53:08 +0330

    add some colors to project

--- MusicChiProj/src/main/java/com/ronaksoftware/musicchi/ui/fragments/HomeViewController.java
+++ MusicChiProj/src/main/java/com/ronaksoftware/musicchi/ui/fragments/HomeViewController.java
@@ -1,11 +1,9 @@
 package com.ronaksoftware.musicchi.ui.fragments;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.view.View;
 
 import com.ronaksoftware.musicchi.ui.presenter.BaseViewController;
-import com.ronaksoftware.musicchi.ui.presenter.Theme;
 
 public class HomeViewController extends BaseViewController {
     @Override
